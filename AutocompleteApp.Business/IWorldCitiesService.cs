3eb5912a6697df24e083db53e1d714d5ea741c76
Tiago Cavaco <tiago.cavaco@gmail.com>
2022-03-06 17:35:22 +0000

    add business project to filter results

---
+++ AutocompleteApp.Business/IWorldCitiesService.cs
@@ -0,0 +1,16 @@
+﻿using AutocompleteApp.Model;
+using System;
+using System.Collections.Generic;
+
+namespace AutocompleteApp.Business
+{
+  public interface IWorldCitiesService
+  {
+    /// <summary>
+    /// Search cities with a name starting with the specified search term, ignores case.
+    /// </summary>
+    /// <param name="searchTerm">Search term to match city names.</param>
+    /// <returns>All cities found.</returns>
+    public IEnumerable<City> SearchCities(string searchTerm);
+  }
+}
---
+++ AutocompleteApp.Business/WorldCitiesService.cs
@@ -0,0 +1,30 @@
+﻿using AutocompleteApp.Data;
+using AutocompleteApp.Model;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AutocompleteApp.Business
+{
+  public class WorldCitiesService : IWorldCitiesService
+  {
+    private readonly IWorldCitiesRepository _repository;
+
+    public WorldCitiesService(IWorldCitiesRepository repository)
+    {
+      _repository = repository;
+    }
+
+    // In case of querying a database the searchTerm should be sanitized to avoid malicious code.
+    public IEnumerable<City> SearchCities(string searchTerm)
+    {
+      if (!string.IsNullOrWhiteSpace(searchTerm))
+        return 
+          _repository
+          .GetAllWorldCities()
+          .Where(x => x.Name.ToLower().StartsWith(searchTerm.ToLower()))
+          .OrderBy(x=> x.Name);
+
+      return new List<City>();
+    }
+  }
+}
