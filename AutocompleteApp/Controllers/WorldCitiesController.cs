47088bd4575b60758c04b6be8f860df9d693bb4b
Tiago Cavaco <tiago.cavaco@gmail.com>
2022-03-06 17:41:04 +0000

    add autocomplete controller

---
+++ AutocompleteApp/Controllers/WorldCitiesController.cs
@@ -0,0 +1,26 @@
+﻿using AutocompleteApp.Business;
+using AutocompleteApp.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace AutocompleteApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WorldCitiesController : ControllerBase
+    {
+        private readonly IWorldCitiesService _service;
+
+        public WorldCitiesController(IWorldCitiesService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        
+        public IEnumerable<City> Get(string searchTerm)
+        {
+            return _service.SearchCities(searchTerm);
+        }
+    }
+}
